fc84d6203ef6bd2ec241d79a94f242bda39da76d
Sylvie-B <unsolo@hotmail.fr>
2021-12-22 22:03:34 +0100

    presque 10 !

--- app.js
+++ app.js
@@ -10,19 +10,29 @@ check.click(function (){
 
 // - 2 -
 
+/** img with no src  **/
 
 // -3 -
-// setInterval(function (){
-//     $('#noel').fadeToggle(500);
-// }, 1000)
+
+setInterval(function (){
+    $('#noel').fadeToggle(500);
+}, 1000)
 
 // -4 -
-$('table')
-
+$('tr:even').css('background', 'lightBlue');
 
 // - 5 -
 
+/** impr text   **/
+
 // - 6 -
+let userMessage = $('textarea');
+userMessage.keypress(function (){
+    console.log()
+    if(userMessage.val().length === userMessage.get(0).maxLength){
+        alert('Vous avez atteint le nombre maximum de caracteres')
+    }
+})
 
 // - 7 -
 let str = $('#text');
@@ -35,11 +45,36 @@ function firstBold (para) {
     // get all string
     let all = para.get(0).innerHTML.split(" ");
 
-    // get first word
+    // get first word in word & suppr first word in all with shift
     let word = all.shift();
-    console.log(all)
 
-    // set para
+    // add first word in bold tag
     para.text("").append("<b>" + word + "</b>");
+
+    // add rest of string
     para.append(" " + all)
 }
+
+// - 8 -
+$('#colorChange').html("<div>Créer un div en utilisant jQuery avec la possibilité de lui ajouter dynamiquement une " +
+    "couleur de fond et une couleur d'écriture</div>").hover(
+    ()=>{
+    $('#colorChange').css({
+    "color" : "white",
+    "backgroundColor" : "blue"})},
+    ()=>{
+    $('#colorChange').css({
+        "color" : "black",
+        "backgroundColor" : ""})
+    })
+
+// - 9 -
+$('#out').css({
+    border : "1px black solid",
+    padding : "1rem"
+});
+
+$('#in').css("border", "1px black solid").appendTo('#out');
+
+//
+
